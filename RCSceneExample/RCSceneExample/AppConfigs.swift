6ef1ecc4405381482a50759c7ac9314d4ffa8967
a1252425 <a1252425@users.noreply.github.com>
2022-04-11 16:26:08 +0800

    Update AppConfigs.swift

--- RCSceneExample/RCSceneExample/AppConfigs.swift
+++ RCSceneExample/RCSceneExample/AppConfigs.swift
@@ -29,12 +29,12 @@ class AppConfigs {
     }
     
     static func configHiFive() {
-        Environment.hiFiveAppId = "6f78321c38ee4db3bb4dae7e56d464b1"
-        Environment.hiFiveServerCode = "ca41ad68e8054610a2"
-        Environment.hiFiveServerVersion = "V4.1.2"
+        Environment.hiFiveAppId = ""
+        Environment.hiFiveServerCode = ""
+        Environment.hiFiveServerVersion = ""
     }
     
     static func configBusinessToken() {
-        Environment.businessToken = "vStHYPdrQoImm-7Ur0ks1g"
+        Environment.businessToken = ""
     }
 }
